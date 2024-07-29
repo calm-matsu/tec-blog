196ace6f2e38d383f01321c1072c853115bcaf8f
calm-matsu <170605104+calm-matsu@users.noreply.github.com>
2024-07-29 22:22:28 +0900

    テストサーバでの実行か本番（ビルド時）かの判定方法を変更

--- src/utils/getCollectionByName.ts
+++ src/utils/getCollectionByName.ts
@@ -1,12 +1,8 @@
 import {getCollection,type ContentEntryMap} from "astro:content";
+import { isProd } from "@utils/isProd";
 
 export const getCollectionByName = async (name: keyof ContentEntryMap) => {
-  let posts = await getCollection(name);
-  if (posts && posts.length > 0 ) {
-    return posts.filter(({data}) => {
-      return import.meta.env.PROD ? !data.draft : true
-    });
-  } else {
-    return []
-  }
+  return await getCollection(name, ({ data }) => {
+    return isProd ? data.draft !== true : true;
+  });
 }
